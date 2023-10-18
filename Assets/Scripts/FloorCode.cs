c86326cbb33eea0194805ec46fbe18c29b01b54e
Deeone78 <deeyimark@gmail.com>
2023-10-18 10:46:42 +0100

    Stuff

--- Assets/Scripts/FloorCode.cs
+++ Assets/Scripts/FloorCode.cs
@@ -6,6 +6,7 @@ public class FloorCode : MonoBehaviour
 {
     bool knockedOver;
     ResetBall resetBall;
+    public GameObject pinCheck;
     // Start is called before the first frame update
     void Awake()
     {
@@ -22,16 +23,26 @@ public class FloorCode : MonoBehaviour
     {
         if (ResetBall.amountOfThrows == 1)
         {
+            
+            
+            if (knockedOver == true)
+            {
+                pinCheck.SetActive(false);
 
+            }
 
         }
     }
     public void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "BowlingAlly") 
+        if (other.gameObject.tag == "BowlingAlly" && knockedOver == false) 
         {
             knockedOver = true; 
             UIManger.AddScore(1);
+            
+
         }
+        
+        
     }
 }
--- Assets/Scripts/PinRespawn.cs
+++ Assets/Scripts/PinRespawn.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class PinRespawn : MonoBehaviour
 {
+  
+    ResetBall resetBall;
+    FloorCode floorCode;
     // Start is called before the first frame update
     private Vector3 startPos;
+
+    void Awake()
+    {
+        resetBall = GetComponent<ResetBall>();
+        floorCode = GetComponent<FloorCode>();
+    }
+
     void Start()
     {
         startPos = transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+       if (ResetBall.amountOfThrows == 1 ) 
+       { 
+        this.transform.position = startPos;
         
+        
+       }
     }
+
+
 }
